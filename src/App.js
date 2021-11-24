6df35789fc36a22a850aa41e153ce9535e1df7c7
Dmitry Dorofeev <d.dorofeev@corp.mail.ru>
2021-11-24 20:21:53 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Deploy S3
+        Deploy simple way
+        I was deployed via Github Actions
       </header>
     </div>
   );
