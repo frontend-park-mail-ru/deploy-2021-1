334bf6cd06fad0947e15c1753f95cd7fd11c3ec1
Dmitry Dorofeev <d.dorofeev@corp.mail.ru>
2022-04-27 21:56:58 +0400

    deploy with scp

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Deploy via S3</h1>
+        <h1>Deploy</h1>
         <p>
           I was deployed via Github Actions
         </p>
