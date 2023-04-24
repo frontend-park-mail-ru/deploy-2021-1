34cbb9e61b64c5156ac56d440398fed086b1b81a
Dmitry Dorofeev <dorof21@yandex.ru>
2023-04-24 15:49:24 +0200

    reset deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Deployed via S3</h1>
+        <h1>Deployed</h1>
         <p>
           I was deployed via Github Actions
         </p>
